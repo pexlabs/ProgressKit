bb80d29af5ce82aee5e5fc330eecadd9ac43de8b
Roland Schemers <roland.schemers@gmail.com>
2016-05-09 09:35:08 -0700

    Add MinimalProgressBar

---
+++ Determinate/MinimalProgressBar.swift
@@ -0,0 +1,49 @@
+//
+//  MinimalProgressBar.swift
+//  ProgressKit
+//
+//  Created by Roland Schemers on 5/9/16.
+//  Copyright © 2016 Kauntey Suryawanshi. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+@IBDesignable
+public class MinimalProgressBar: DeterminateAnimation {
+
+    var progressLayer = CAShapeLayer()
+
+    @IBInspectable public var borderColor: NSColor = NSColor.blackColor() {
+        didSet {
+            notifyViewRedesigned()
+        }
+    }
+
+    override func notifyViewRedesigned() {
+        super.notifyViewRedesigned()
+        progressLayer.backgroundColor = foreground.CGColor
+    }
+
+    override func configureLayers() {
+        super.configureLayers()
+
+        progressLayer.frame = bounds
+        progressLayer.cornerRadius = progressLayer.frame.height / 2
+        progressLayer.backgroundColor = foreground.CGColor
+        layer?.addSublayer(progressLayer)
+    }
+
+    override func updateProgress() {
+        CATransaction.begin()
+        if animated {
+            CATransaction.setAnimationDuration(0.5)
+        } else {
+            CATransaction.setDisableActions(true)
+        }
+        let timing = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        CATransaction.setAnimationTimingFunction(timing)
+        progressLayer.frame.size.width = bounds.width * progress
+        CATransaction.commit()
+    }
+}
